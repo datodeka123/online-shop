2fc0141c2ac11ff0e8467268a5b0d79dcef321c8
daviti dekanoidze <dekanoidze.d1@gtu.ge>
2024-06-21 21:01:16 +0400

    part 8

--- src/email/OrderHistory.tsx
+++ src/email/OrderHistory.tsx
@@ -25,35 +25,6 @@ type OrderHistoryEmailProps = {
   }[];
 };
 
-OrderHistoryEmail.PreviewProps = {
-  orders: [
-    {
-      id: crypto.randomUUID(),
-      createdAt: new Date(),
-      pricePaidInCents: 10000,
-      downloadVerificationId: crypto.randomUUID(),
-      product: {
-        name: "Product name",
-        description: "Some description",
-        imagePath:
-          "/products/5aba7442-e4a5-4d2e-bfa7-5bd358cdad64-02 - What Is Next.js.jpg",
-      },
-    },
-    {
-      id: crypto.randomUUID(),
-      createdAt: new Date(),
-      pricePaidInCents: 2000,
-      downloadVerificationId: crypto.randomUUID(),
-      product: {
-        name: "Product name 2",
-        description: "Some other desc",
-        imagePath:
-          "/products/db3035a5-e762-41b0-996f-d54ec730bc9c-01 - Course Introduction.jpg",
-      },
-    },
-  ],
-} satisfies OrderHistoryEmailProps;
-
 export default function OrderHistoryEmail({ orders }: OrderHistoryEmailProps) {
   return (
     <Html>
