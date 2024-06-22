2125772a9f70c2a9dc3e82f8ddf730b2a648bb65
daviti dekanoidze <dekanoidze.d1@gtu.ge>
2024-06-22 15:11:52 +0400

    part 11

---
+++ src/app/(customerFacing)/products/components/ProductsSuspense.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { ProductCard } from "@/components/ProductCard";
+import { useSearchContext } from "./SearchContextProvider";
+
+export default function ProductsSuspense({
+  products,
+}: {
+  products: {
+    id: string;
+    name: string;
+    priceInCents: number;
+    filePath: string;
+    imagePath: string;
+    description: string;
+    isAvailableForPurchase: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+  }[];
+}) {
+  const { search } = useSearchContext();
+  const filteredProducts = products.filter((product) =>
+    product.name.toUpperCase().includes(search.toUpperCase())
+  );
+
+  if (search) {
+    return filteredProducts.map((product) => (
+      <ProductCard key={product.id} {...product} />
+    ));
+  } else {
+    return products.map((product) => (
+      <ProductCard key={product.id} {...product} />
+    ));
+  }
+}
--- src/app/(customerFacing)/products/page.tsx
+++ src/app/(customerFacing)/products/page.tsx
@@ -1,40 +1,31 @@
-import { ProductCard, ProductCardSkeleton } from "@/components/ProductCard";
-import db from "@/db/db";
-import { cache } from "@/lib/cache";
+import { ProductCardSkeleton } from "@/components/ProductCard";
 import { Suspense } from "react";
+import ProductsSuspense from "./components/ProductsSuspense";
+import Search from "./components/Search";
+import { getProducts } from "./lib/getProducts";
 
-const getProducts = cache(() => {
-  return db.product.findMany({
-    where: { isAvailableForPurchase: true },
-    orderBy: { name: "asc" },
-  });
-}, ["/products", "getProducts"]);
-
-export default function ProductsPage() {
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      <Suspense
-        fallback={
-          <>
-            <ProductCardSkeleton />
-            <ProductCardSkeleton />
-            <ProductCardSkeleton />
-            <ProductCardSkeleton />
-            <ProductCardSkeleton />
-            <ProductCardSkeleton />
-          </>
-        }
-      >
-        <ProductsSuspense />
-      </Suspense>
-    </div>
-  );
-}
-
-async function ProductsSuspense() {
+export default async function ProductsPage() {
   const products = await getProducts();
 
-  return products.map((product) => (
-    <ProductCard key={product.id} {...product} />
-  ));
+  return (
+    <>
+      <Search />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <Suspense
+          fallback={
+            <>
+              <ProductCardSkeleton />
+              <ProductCardSkeleton />
+              <ProductCardSkeleton />
+              <ProductCardSkeleton />
+              <ProductCardSkeleton />
+              <ProductCardSkeleton />
+            </>
+          }
+        >
+          <ProductsSuspense products={products} />
+        </Suspense>
+      </div>
+    </>
+  );
 }
