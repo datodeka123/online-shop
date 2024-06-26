7ff4b60f3bd449ddb043874b39e8f65e2f310cbb
daviti dekanoidze <dekanoidze.d1@gtu.ge>
2024-06-26 17:15:15 +0400

    change site name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import { cn } from "@/lib/utils";
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
-  title: "ონლაინ მაღაზია",
-  description: "ვიდეო მასალის ონლაინ გაყიდვა",
+  title: "ონლაინ კურსები",
+  description: "საუკეთესო ონლაინ კურსების მაღაზია",
 };
 
 export default function RootLayout({
